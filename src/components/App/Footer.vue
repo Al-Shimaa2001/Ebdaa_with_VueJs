c5ded30694b8a996c3355c08dbc10c8635420501
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-12-11 22:41:38 +0200

    add classes of style , add router to atterbute , make date change dynamic

--- src/components/App/Footer.vue
+++ src/components/App/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer
-    class="bg-[#E6EDEE] border-2 border-[#B0C7CB] rounded-tr-2xl rounded-tl-2xl p-10 h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+    class="bg-[#E6EDEE] border-t-2 border-t-[#B0C7CB] rounded-tr-[50px] rounded-tl-[50px] p-10 h-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8"
   >
     <div>
-      <img src="/public/images/Logo.png" alt="logo" width="138" height="40" />
+      <img src="/public/images/Logo.png" alt="logo" width="138" height="40" class="mb-6" />
       <p class="w-[405px] h-[108] text-description">
         تيكزون، متجر إلكتروني في المملكة العربية السعودية في جدة ويقدم العديد من المنتجات
         الإلكترونية من قطع وإكسسوارات وألعاب الأطفال, وغيرها من تسهيلات الحياة، خدماتنا لجميع دول
@@ -11,16 +11,25 @@
       </p>
     </div>
     <div>
-      <h3>روابط مهمه</h3>
-      <span>سياسه الاستخدام والخصوصيه</span>
-      <span>سياسه الاستبدال والاسترجاع</span>
+      <h3 class="mb-4 font-bold text-2xl">روابط مهمه</h3>
+      <div class="flex gap-8">
+        <router-link to="/">سياسه الاستخدام والخصوصيه</router-link>
+        <router-link to="/">سياسه الاستبدال والاسترجاع</router-link>
+      </div>
     </div>
     <div>
       <h3>تواصل معنا</h3>
     </div>
   </footer>
+  <div class="bg-[#D9E4E6] py-6 pr-20 text-2xl font-bold">
+    <p>الحقوق محفوظه | TEQZON {{ dateOfYear }}</p>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+const dateOfYear = ref(new Date().getFullYear())
+</script>
 
 <style scoped></style>
