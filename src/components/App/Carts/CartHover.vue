808ff9698cb04c7910818c1fc0341caae5150e86
Alshimaa_JamalAL-Shimaa-Jamal <alshimaajamal@std.mans.edu.eg>
2025-12-11 21:41:12 +0200

    finish hovering section

--- src/components/App/Carts/CartHover.vue
+++ src/components/App/Carts/CartHover.vue
@@ -1,7 +1,8 @@
 <template>
   <div
+    v-if="hoverCart"
     class="absolute top-44 bg-primary text-white rounded-xl flex justify-center items-center h-12 w-5/6"
-    :class="{ 'opacity-100 visible': hoverCart, 'opacity-0 invisible': !hoverCart }"
+    :class="{ 'opacity-100 visible cursor-pointer': hoverCart, 'opacity-0 invisible': !hoverCart }"
   >
     <Icon icon="uil:cart" width="24" height="24" style="color: #ffffff" />
     <p>اضف للسله</p>
